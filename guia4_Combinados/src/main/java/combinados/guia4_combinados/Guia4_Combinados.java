57c614a2a68325a817fc8b952ef833180295695f
LaimeFacundo <128554220+LaimeFacundo@users.noreply.github.com>
2023-04-27 16:22:35 -0300

    Guia 4 carpeta

---
+++ guia4_Combinados/src/main/java/combinados/guia4_combinados/Guia4_Combinados.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package combinados.guia4_combinados;
+
+/**
+ *
+ * @author yofac
+ */
+public class Guia4_Combinados {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
